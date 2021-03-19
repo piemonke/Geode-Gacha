4814781d60775554cff210b172df21aa8b8433c8
Dylan Sharon <reachdylan@gmail.com>
2021-03-19 14:21:14 -0400

    setup .env file and connect .env to server.js

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const morgan = require("morgan");
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+const port = process.env.PORT;
+
+const connectionURI = process.env.MONGODB_URI; 
+
+// const session = require("express-session");
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
+
+
+
+app.listen(port, function() {
+    console.log(connectionURI);
+    console.log(`Express is listening on port:${port}`);
+});
